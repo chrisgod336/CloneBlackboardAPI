cc2f37555b0ee0833be3ce5da466e193f05add10
Christian <christiansoares336@gmail.com>
2025-06-02 18:42:45 -0300

    merge adaptacoes para rodar no notebook

--- src/services/api.js
+++ src/services/api.js
@@ -4,11 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
-const baseURL = 'http://localhost:5000/';
 const api = axios_1.default.create({
-    baseURL,
+    baseURL: 'http://127.0.0.1:5000/', // Use IPV4 para evitar problemas
+    timeout: 10000, // 10 segundos de timeout
     headers: {
         'Content-Type': 'application/json',
     },
+});
+// Interceptador para logs de depuração
+api.interceptors.request.use(config => {
+    console.log('Enviando requisição:', config);
+    return config;
+});
+api.interceptors.response.use(response => {
+    console.log('Resposta recebida:', response);
+    return response;
+}, error => {
+    console.error('Erro na resposta:', error);
+    return Promise.reject(error);
 });
 exports.default = api;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,25 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:5000/'; 
-
 const api = axios.create({
-  baseURL,
+  baseURL: 'http://127.0.0.1:5000/', // Use IPV4 para evitar problemas
+  timeout: 10000, // 10 segundos de timeout
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+// Interceptador para logs de depuração
+api.interceptors.request.use(config => {
+  console.log('Enviando requisição:', config);
+  return config;
+});
+
+api.interceptors.response.use(response => {
+  console.log('Resposta recebida:', response);
+  return response;
+}, error => {
+  console.error('Erro na resposta:', error);
+  return Promise.reject(error);
 });
 
 export default api;
